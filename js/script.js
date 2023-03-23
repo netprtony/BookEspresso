252c1dbfac89ef9621dff99cc77a3a9d50160286
unknown <huynhvikhang913@gmail.com>
2023-03-23 21:14:32 +0700

    9:14

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let popup  = document.getElementById("popup");
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function search_book(){
+    popup.classList.remove("open-popup");
+}
