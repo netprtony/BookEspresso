88f5ff7010699967d31320c4ea57f8e72094ed14
unknown <huynhvikhang913@gmail.com>
2023-03-25 21:34:23 +0700

    25/3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-let popup  = document.getElementById("popup");
-function openPopup(){
-    popup.classList.add("open-popup");
-}
-function search_book(){
-    popup.classList.remove("open-popup");
-}
+
