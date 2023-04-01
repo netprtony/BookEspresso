6616bce161fc7dd3f9593de48f0b2634e8c9bb35
unknown <huynhvikhang913@gmail.com>
2023-04-01 07:52:59 +0700

    1/4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+function changeImg(smallImg){
+    var fullImg = document.getElementById("imgBox");
+    fullImg.src = smallImg.src;
+    fullImg.style.width = "auto";
+    fullImg.style.height = "500px";
+}
